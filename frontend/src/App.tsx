1ec3c22734146a1eaa974c9108cce6f456d980d6
Aditya Cahyo <aditydcp@gmail.com>
2024-09-19 20:13:01 +0700

    add route protection

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,11 +6,20 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import Login from "./pages/auth/Login"
+import ProtectedRoute from "./components/utils/protected-routes"
 
 function App() {
+  const isAuthenticated = Boolean(localStorage.getItem("token"))
+
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route
+        path="/"
+        element={<ProtectedRoute isAuthenticated={isAuthenticated}>
+          <Home />
+        </ProtectedRoute>
+        }
+      />
       <Route path="/login" element={<Login />} />
     </Routes>
   )
